6837cdd0c451881dfbe747e8f730a3e20508b122
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-08-11 18:40:34 +0200

    aug 11 2

--- date_struc/src/main.rs
+++ date_struc/src/main.rs
@@ -1,28 +1,9 @@
+extern crate chrono;
 
-use chrono::{Datelike, Utc};
-
+use chrono::prelude::*;
 
 fn main() {
+    let date = Utc::today().format("%Y-%m-%d");
 
-    let now = Utc::now();
-
-
-    //let (is_common_era, year) = now.year_ce();
-    println!(
-        "The current UTC date is {}-{:02}-{:02} {:?}",
-        now.year(),
-        now.month(),
-        now.day(),
-        now.weekday(),
-        //if is_common_era { "CE" } else { "BCE" }
-    );
-
-    struct Date {
-        date_str: String
-        unix_timestamp: 
-     
-    }
-
-
-
+    println! ("Today is {}", date);
 }
