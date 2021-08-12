4d72add6522ac142ac710e58a855a90bf9dbac36
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-08-12 23:03:00 +0200

    aug 12 5

--- date_struc/src/main.rs
+++ date_struc/src/main.rs
@@ -10,22 +10,46 @@ fn main() {
     }
 
     impl Date {
-        fn get_formatted_date_str (&mut self) {
+        fn get_formatted_date_str (&mut self) -> String{
+            let day = self.date_str.day();
+            let month = self.date_str.month();
+            let year = self.date_str.year();
+
+
+            let month_str = String::new();
+
+            println!("{:?}", month);
+
+            match month {
+                1 => month_str.push("January"),
+                2 => "Febraury"
+                3 => "March"
+                4 => "April"
+                5 => "May"
+                6 => "June"
+                7 => "July"
+                8 => "August"
+                9 => "September"
+                10 => "October"
+                11 => "November"
+                12 => "December"
+                _=>println!("nothing"),
+            }
 
 
         }
     }
 
-    let new_date = Date {
+    let mut new_date = Date {
         date_str: Local::now(),
         unix_timestamp: SystemTime::now(),
     };
 
     let date_formated = new_date.date_str.format("%Y-%m-%d");
-    println!("{}", date_formated);
+    println!("{:?}", date_formated);
     println!("{:?}", new_date.unix_timestamp);
 
-    
+    new_date.get_formatted_date_str();
 
 
 }
