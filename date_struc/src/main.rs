709c97b64831ce2ab89169e870108248a3d29840
Aitor Zaldua <aitor.zaldua@gmail.com>
2021-08-13 18:09:38 +0200

    aug 13 2

--- date_struc/src/main.rs
+++ date_struc/src/main.rs
@@ -11,11 +11,11 @@ fn main() {
 
     impl Date {
         fn get_formatted_date_str (&mut self) -> String {
-            let day = self.date_str.day();
+            let day = self.date_str.day().to_string();
             let month = self.date_str.month();
-            let year = self.date_str.year();
+            let year = self.date_str.year().to_string();
 
-            match month {
+            let month_str = match month {
                 1 => "January".to_string(),
                 2 => "Febraury".to_string(),
                 3 => "March".to_string(),
@@ -29,23 +29,52 @@ fn main() {
                 11 => "November".to_string(),
                 12 => "December".to_string(),
                 _=> "panic! ".to_string(),
-            }
+            };
 
+
+            let mut date_formatted = String::new();
+            date_formatted.push_str(&day);
+            date_formatted += " ";
+            date_formatted.push_str(&month_str);
+            date_formatted += " ";
+            date_formatted.push_str(&year);
+
+            date_formatted
+
+
+        }
+
+        fn get_date_str (&mut self) -> String{
+
+            let d = self.date_str.format("%Y-%m-%d");
+
+            d.to_string()
 
         }
     }
 
-    let mut new_date = Date {
+
+    let mut new = Date {
         date_str: Local::now(),
         unix_timestamp: SystemTime::now(),
     };
 
-    let date_formated = new_date.date_str.format("%Y-%m-%d");
-    println!("{}", date_formated);
-    println!("{:?}", new_date.unix_timestamp);
 
-    println!("{}", new_date.get_formatted_date_str());
+    //Create an initializer function (constructor) named new, such that you can create a Date with the following syntax: Date::new("2021-08-25")
+    let d = new.date_str.format("%Y-%m-%d");
 
+    println!("{}", d);
+
+    //Implement a function get_formatted_date_str that returns a date string formatted as following: "2. August 2021"
+
+    println!("{}", new.get_formatted_date_str());
+
+
+    //Implement a function get_date_str that returns the date_str of the date: "2021-08-02":
+    println!("{}",new.get_date_str());
+
+    //Implement a function get_unix that returns the unix timestamp of the date: 1627862400
+    println!("{:?}", new.unix_timestamp);
 
 
 
